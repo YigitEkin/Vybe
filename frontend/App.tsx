6a3a2837af55157a0801a4be29bacf8b86bdcdf6
Can Önal <can.onal@ug.bilkent.edu.tr>
2022-12-10 20:25:12 +0300

    Login Signup Verification Page
    New feature Login Signup Verification

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,29 +1,30 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import HomePage from "./screens/HomePage";
-import { useCallback } from "react";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import SignUpMail from "./screens/signup/SignUpMail";
-import SignUpCompletedScreen from "./screens/signup/SignUpCompletedScreen";
-import SignUpPassword from "./screens/signup/SignUpPassword";
-import SignUpUsername from "./screens/signup/SignupUsername";
-import EnterPhoneNumber from "./screens/2fa/EnterPhoneNumber";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import HomePage from './screens/HomePage';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import SignUpMail from './screens/signup/SignUpMail';
+import SignUpCompletedScreen from './screens/signup/SignUpCompletedScreen';
+import SignUpPassword from './screens/signup/SignUpPassword';
+import SignUpUsername from './screens/signup/SignupUsername';
+import EnterPhoneNumber from './screens/2fa/EnterPhoneNumber';
+import LoginVerification from './screens/Verification/LoginVerification';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    "Inter-Black": require("./assets/fonts/Inter/static/Inter-Black.ttf"),
-    "Inter-Bold": require("./assets/fonts/Inter/static/Inter-Bold.ttf"),
-    "Inter-ExtraBold": require("./assets/fonts/Inter/static/Inter-ExtraBold.ttf"),
-    "Inter-ExtraLight": require("./assets/fonts/Inter/static/Inter-ExtraLight.ttf"),
-    "Inter-Light": require("./assets/fonts/Inter/static/Inter-Light.ttf"),
-    "Inter-Medium": require("./assets/fonts/Inter/static/Inter-Medium.ttf"),
-    "Inter-Regular": require("./assets/fonts/Inter/static/Inter-Regular.ttf"),
-    "Inter-SemiBold": require("./assets/fonts/Inter/static/Inter-SemiBold.ttf"),
-    "Inter-Thin": require("./assets/fonts/Inter/static/Inter-Thin.ttf"),
+    'Inter-Black': require('./assets/fonts/Inter/static/Inter-Black.ttf'),
+    'Inter-Bold': require('./assets/fonts/Inter/static/Inter-Bold.ttf'),
+    'Inter-ExtraBold': require('./assets/fonts/Inter/static/Inter-ExtraBold.ttf'),
+    'Inter-ExtraLight': require('./assets/fonts/Inter/static/Inter-ExtraLight.ttf'),
+    'Inter-Light': require('./assets/fonts/Inter/static/Inter-Light.ttf'),
+    'Inter-Medium': require('./assets/fonts/Inter/static/Inter-Medium.ttf'),
+    'Inter-Regular': require('./assets/fonts/Inter/static/Inter-Regular.ttf'),
+    'Inter-SemiBold': require('./assets/fonts/Inter/static/Inter-SemiBold.ttf'),
+    'Inter-Thin': require('./assets/fonts/Inter/static/Inter-Thin.ttf'),
   });
 
   const onLayoutRootView = useCallback(async () => {
@@ -39,11 +40,15 @@ export default function App() {
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
       <SafeAreaView style={styles.container}>
-        <StatusBar style="light" networkActivityIndicatorVisible={true} />
-        <EnterPhoneNumber
+        <StatusBar style='light' networkActivityIndicatorVisible={true} />
+        {/*<EnterPhoneNumber
           headerText="Welcome Back"
           subHeaderText="Log in to your account"
           buttonText="Log in"
+        />*/}
+        <LoginVerification
+          headerText='Enter authentication code'
+          phoneNumber='+1 222-828292'
         />
       </SafeAreaView>
     </View>
@@ -53,8 +58,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000",
-    alignItems: "center",
-    width: "100%",
+    backgroundColor: '#000',
+    alignItems: 'center',
+    width: '100%',
   },
 });
---
+++ frontend/screens/Verification/LoginVerification.tsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  TouchableHighlight,
+} from 'react-native';
+import * as Font from 'expo-font';
+import StyledButton from '../../components/HomePage/StyledButton';
+import OTPTextView from 'react-native-otp-textinput';
+
+type LoginVerificationProps = {
+  headerText: string;
+
+  phoneNumber: string;
+};
+
+const LoginVerification = ({
+  headerText,
+  //  buttonText,
+  phoneNumber,
+}: LoginVerificationProps) => {
+  const [OTPCode, setOTPCode] = useState(0);
+  const handleChange = (e) => {
+    setOTPCode(e);
+  };
+  const [fontsLoaded] = Font.useFonts({
+    'Inter-Bold': require('../../assets/fonts/Inter/static/Inter-Bold.ttf'),
+    'Inter-Medium': require('../../assets/fonts/Inter/static/Inter-Medium.ttf'),
+    'Inter-Regular': require('../../assets/fonts/Inter/static/Inter-Regular.ttf'),
+  });
+  return fontsLoaded ? (
+    <KeyboardAvoidingView behavior='padding' style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.formAreaContainer}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.headerText}>{headerText + '.'}</Text>
+            <Text style={styles.subHeaderText}>
+              Enter the 4-digit that we have sent via the phone number{' '}
+              {phoneNumber}
+            </Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <OTPTextView
+              //style={styles.roundedTextInput}
+              offTintColor={'#303437'}
+              tintColor={'#6B4EFF'}
+              //containerStyle={styles.roundedTextInput}
+              textInputStyle={[styles.textStyle, styles.roundedTextInput]}
+              handleTextChange={(e) => handleChange(e)}
+            ></OTPTextView>
+          </View>
+        </View>
+        <View style={styles.StyledButton}>
+          <StyledButton
+            style={styles.StyledButton}
+            buttonText='Continue'
+            onPress={() => console.log(OTPCode)}
+          />
+          <TouchableHighlight onPress={() => console.log('Pressed')}>
+            <Text style={styles.resendText}>{'Resend Code'}</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  ) : null;
+};
+const styles = StyleSheet.create({
+  headerText: {
+    fontFamily: 'Inter-Bold',
+    fontSize: 24,
+    color: '#fff',
+    textAlign: 'center',
+  },
+  subHeaderText: {
+    marginTop: 5,
+    fontFamily: 'Inter-Regular',
+    fontSize: 16,
+    color: '#979C9E',
+    textAlign: 'center',
+  },
+  StyledButton: {
+    marginTop: 'auto',
+    marginBottom: 20,
+    width: '100%',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    //justifyContent: 'center',
+    alignItems: 'center',
+    width: '95%',
+  },
+  formAreaContainer: {
+    width: '100%',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: '30%',
+  },
+  inputContainer: {
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  roundedTextInput: {
+    borderRadius: 100,
+    borderWidth: 2,
+    borderBottomWidth: 2,
+  },
+  textStyle: {
+    color: '#FFFFFF',
+  },
+  resendText: {
+    color: '#9990FF',
+    fontFamily: 'Inter-Regular',
+    fontSize: 16,
+  },
+});
+export default LoginVerification;
