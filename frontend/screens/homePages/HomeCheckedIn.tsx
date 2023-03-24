bb2a326fd2ba5593b7489d0c8d0a108367e47b7d
Can Önal <can.onal@ug.bilkent.edu.tr>
2023-03-24 21:48:04 +0300

    Add song to queue and home checked in screens
    Home checked in and add to queue screens have been implemented, currently, most of the functionalities are not implemented.

--- frontend/screens/homePages/HomeCheckedIn.tsx
+++ frontend/screens/homePages/HomeCheckedIn.tsx
@@ -6,200 +6,306 @@ import {
   Pressable,
   Image,
   Alert,
-} from "react-native";
-import React, { useEffect, useState } from "react";
-import GroupItem from "../../components/HomePage/GroupItem";
-import { Colors } from "../../constants/Colors";
-import { Camera, CameraType } from "expo-camera";
+  ScrollView,
+  Modal,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import GroupItem from '../../components/HomePage/GroupItem';
+import { Colors } from '../../constants/Colors';
+import { Camera, CameraType } from 'expo-camera';
+import FAAddSongToQueue from '../../components/HomePage/FAAddSongToQueue';
+import SearchBar from '../../components/HomePage/SearchBar';
 
-import FriendItem from "../../components/HomePage/FriendItem";
-import FAButton from "../../components/HomePage/FAButton";
-import SearchIcon from "../../assets/SearchIcon.png";
-import Icon from "react-native-vector-icons/Ionicons";
-import * as Font from "expo-font";
-import { BarCodeScanner } from "expo-barcode-scanner";
+import ListItem from '../../components/HomePage/ListItem';
+import FAButton from '../../components/HomePage/FAButton';
+import SearchIcon from '../../assets/SearchIcon.png';
+import Icon from 'react-native-vector-icons/Ionicons';
+import EvilIcon from 'react-native-vector-icons/EvilIcons';
+import * as Font from 'expo-font';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import FABCheckout from '../../components/HomePage/FABCheckout';
 
 const HomeCheckedIn = () => {
   const [isCamOpen, setIsCamOpen] = useState(false);
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
-  const [startCamera, setStartCamera] = React.useState(false);
+  const [addSong, setAddSong] = React.useState(false);
+  const [searchPhrase, setSearchPhrase] = useState('');
+  const [clicked, setClicked] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {}, []);
-  const __startCamera = async () => {
-    const { status } = await Camera.requestCameraPermissionsAsync();
+  const __addSongToQueue = async () => {
     //console.log(status);
-    if (status === "granted") {
-      setStartCamera(true);
-    } else {
-      Alert.alert("Please grant access to camera from device settings!");
-    }
+
+    setAddSong(true);
+  };
+
+  const __checkout = async () => {
+    setModalVisible(true);
   };
 
   const [fontsLoaded] = Font.useFonts({
-    "Inter-Bold": require("../../assets/fonts/Inter/static/Inter-Bold.ttf"),
-    "Inter-Medium": require("../../assets/fonts/Inter/static/Inter-Medium.ttf"),
-    "Inter-Regular": require("../../assets/fonts/Inter/static/Inter-Regular.ttf"),
+    'Inter-Bold': require('../../assets/fonts/Inter/static/Inter-Bold.ttf'),
+    'Inter-Medium': require('../../assets/fonts/Inter/static/Inter-Medium.ttf'),
+    'Inter-Regular': require('../../assets/fonts/Inter/static/Inter-Regular.ttf'),
   });
-  const data = [
-    {
-      text: "Deneme 1",
-    },
-    {
-      text: "Deneme 2",
-    },
-    {
-      text: "Deneme 3",
-    },
-    {
-      text: "Deneme 4",
-    },
-    {
-      text: "Deneme 5",
-    },
-    {
-      text: "Deneme 6",
-    },
-    {
-      text: "Deneme 7",
-    },
-    {
-      text: "Deneme 8",
-    },
-  ];
-  return !startCamera ? (
+
+  return !addSong ? (
     <View style={styles.container}>
       <View
         style={{
           flex: 1,
-          width: "100%",
+          width: '100%',
         }}
       >
-        <View
-          style={{
-            flexDirection: "row",
-            marginVertical: 20,
-            alignItems: "center",
+        <Modal
+          animationType='fade'
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
           }}
         >
-          <View style={styles.textContainer}>
-            <Text style={styles.textStyle}>{"You are not Vybing!"}</Text>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Checking Out</Text>
+              <View style={{ flex: 1, justifyContent: 'space-around' }}>
+                <Text style={styles.modalTextStyle}>
+                  {'Are you sure you want to checkout from this location?'}
+                </Text>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text
+                    style={{
+                      fontFamily: 'Inter-Regular',
+                      color: 'white',
+                      fontSize: 20,
+                    }}
+                  >
+                    Yes I'm Sure
+                  </Text>
+                </Pressable>
+                <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                  <Text style={styles.modalTextStyle}>
+                    {'I changed my mind'}
+                  </Text>
+                </Pressable>
+              </View>
+            </View>
           </View>
-          <View style={{ marginLeft: "auto" }}>
-            <Pressable
-              style={({ pressed }) =>
-                pressed ? [styles.pressed] : [styles.buttonContainer]
-              }
-              android_ripple={{ color: "#000" }}
-            >
-              <Image source={SearchIcon} />
-            </Pressable>
+        </Modal>
+        <View
+          style={{
+            flexDirection: 'row',
+            marginVertical: 20,
+            alignItems: 'center',
+          }}
+        >
+          <View>
+            <View style={styles.textContainer}>
+              <Text style={styles.textStyle}>{'You are Now Vybing!'}</Text>
+              <View style={{ marginLeft: 'auto' }}>
+                <Pressable
+                  style={({ pressed }) =>
+                    pressed ? [styles.pressed] : [styles.buttonContainer]
+                  }
+                  android_ripple={{ color: '#000' }}
+                >
+                  <Image source={SearchIcon} />
+                </Pressable>
+              </View>
+            </View>
+            <View style={styles.textContainer}>
+              <View style={{ paddingLeft: 8, justifyContent: 'center' }}>
+                <EvilIcon name='location' color={'#7c757e'} size={30} />
+              </View>
+              <Text style={styles.locationTextStyle}>
+                {'Location will be displayed here'}
+              </Text>
+            </View>
           </View>
         </View>
-        <FlatList
-          data={data}
-          renderItem={({ item }) => <GroupItem text={item.text} />}
-          horizontal={true}
-          style={{ flexGrow: 0, marginBottom: 100 }}
-        />
-        <Text style={[styles.textStyle, { marginBottom: 20 }]}>
-          {"Friends currently Vybing"}
+
+        <Text style={[styles.textStyle, { marginBottom: 20, fontSize: 23 }]}>
+          {'Current Song Queue'}
         </Text>
-        <FriendItem />
+        <ScrollView style={{ height: '100%', marginBottom: 100 }}>
+          <ListItem topText={'Song Name'} subText={'Artist Name'} />
+          <ListItem topText={'Song Name'} subText={'Artist Name'} />
+          <ListItem topText={'Song Name'} subText={'Artist Name'} />
+          <ListItem topText={'Song Name'} subText={'Artist Name'} />
+          <ListItem topText={'Song Name'} subText={'Artist Name'} />
+          <ListItem topText={'Song Name'} subText={'Artist Name'} />
+          <ListItem topText={'Song Name'} subText={'Artist Name'} />
+          <ListItem topText={'Song Name'} subText={'Artist Name'} />
+        </ScrollView>
       </View>
 
-      <FAButton
-        style={{ zIndex: 100, bottom: 100, position: "absolute" }}
-        onPress={__startCamera}
+      <FAAddSongToQueue
+        buttonText={'Add Song To Queue'}
+        style={{ zIndex: 100, bottom: 180, position: 'absolute' }}
+        onPress={__addSongToQueue}
+      />
+      <FABCheckout
+        buttonText={'Checkout'}
+        style={{ zIndex: 100, bottom: 100, position: 'absolute' }}
+        onPress={__checkout}
       />
     </View>
   ) : (
     <View
       style={{
         flex: 1,
-        width: "100%",
+        width: '100%',
       }}
     >
-      <View></View>
-      <Camera
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        type={CameraType.back}
-        barCodeScannerSettings={{
-          barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
+      <Modal
+        animationType='fade'
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
         }}
-        onBarCodeScanned={(result: any) => console.log(result.data)}
       >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Checking Out</Text>
+            <View style={{ flex: 1, justifyContent: 'space-around' }}>
+              <Text style={styles.modalTextStyle}>
+                {'Are you sure you want to checkout from this location?'}
+              </Text>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text
+                  style={{
+                    fontFamily: 'Inter-Regular',
+                    color: 'white',
+                    fontSize: 20,
+                  }}
+                >
+                  Yes I'm Sure
+                </Text>
+              </Pressable>
+              <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.modalTextStyle}>{'I changed my mind'}</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <View style={{ alignItems: 'center', flexDirection: 'row' }}>
         <Pressable
-          onPress={() => setStartCamera(false)}
+          onPress={() => setAddSong(false)}
           style={({ pressed }) =>
             pressed ? [styles.backButtonPressed] : [styles.backButton]
           }
         >
-          <Icon name="chevron-back" color="#fff" size={40} />
+          <Icon name='chevron-back' color='#fff' size={40} />
         </Pressable>
-        <View
-          style={{
-            width: 250,
-            height: 250,
-            borderWidth: 4,
-            borderColor: "#fff",
-            borderRadius: 35,
-            marginBottom: 10,
-          }}
-        ></View>
-        <Text style={styles.textStyle}>{"Scan QR"}</Text>
-      </Camera>
+        <View>
+          <Text style={[styles.textStyle, { fontSize: 23 }]}>
+            {'Add Song To Queue'}
+          </Text>
+        </View>
+      </View>
+      <SearchBar
+        searchPhrase={searchPhrase}
+        clicked={clicked}
+        setClicked={setClicked}
+        setSearchPhrase={setSearchPhrase}
+      />
+      <ScrollView style={{ height: '100%', marginBottom: 100 }}>
+        <ListItem topText={'Song Name'} subText={'Artist Name'} />
+        <ListItem topText={'Song Name'} subText={'Artist Name'} />
+        <ListItem topText={'Song Name'} subText={'Artist Name'} />
+        <ListItem topText={'Song Name'} subText={'Artist Name'} />
+        <ListItem topText={'Song Name'} subText={'Artist Name'} />
+        <ListItem topText={'Song Name'} subText={'Artist Name'} />
+        <ListItem topText={'Song Name'} subText={'Artist Name'} />
+        <ListItem topText={'Song Name'} subText={'Artist Name'} />
+      </ScrollView>
+      <View style={{ alignItems: 'center' }}>
+        <FABCheckout
+          buttonText={'Checkout'}
+          style={{ zIndex: 100, bottom: 100, position: 'absolute' }}
+          onPress={__checkout}
+        />
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
   flatList: {
     //flex: 1,
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
   },
   container: {
     flex: 1,
-    width: "100%",
-    alignItems: "center",
+    width: '100%',
+    alignItems: 'center',
   },
   textStyle: {
-    fontFamily: "Inter-Bold",
+    fontFamily: 'Inter-Bold',
     fontSize: 24,
-    color: "#fff",
+    color: '#fff',
     //lineHeight: 20,
 
     paddingLeft: 8,
   },
+  locationTextStyle: {
+    fontFamily: 'Inter-Regular',
+    fontSize: 18,
+    color: '#7C757E',
+    paddingLeft: 8,
+  },
   textContainer: {
-    flexDirection: "row",
-    justifyContent: "flex-start",
+    flexDirection: 'row',
+    width: '100%',
+    //justifyContent: 'flex-start',
   },
   buttonContainer: {
-    width: 20,
-    height: 20,
+    width: 30,
+    height: 30,
     //backgroundColor: Colors.red.error,
     borderRadius: 100,
-    justifyContent: "center",
-    alignItems: "center",
-    //marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20,
   },
   backButton: {
-    position: "absolute",
+    //position: 'absolute',
     width: 60,
     height: 60,
-    marginTop: 10,
-    marginLeft: 10,
-    top: 10,
-    left: 10,
+    justifyContent: 'center',
+    //marginTop: 10,
+    //marginLeft: 10,
+    //marginBottom: 20,
+    //top: 10,
+    //left: 10,
   },
   backButtonPressed: {
-    position: "absolute",
+    //position: 'absolute',
     width: 60,
     height: 60,
-    marginTop: 10,
-    marginLeft: 10,
-    top: 10,
-    left: 10,
+    justifyContent: 'center',
+    //marginTop: 10,
+    //marginLeft: 10,
+    //marginBottom: 20,
+    //top: 10,
+    //left: 10,
     opacity: 0.5,
   },
   pressed: {
@@ -207,10 +313,50 @@ const styles = StyleSheet.create({
     height: 20,
     //backgroundColor: Colors.red.error,
     borderRadius: 100,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     //marginTop: 20,
     opacity: 0.5,
+  },
+  modalView: {
+    margin: 20,
+    width: 400,
+    height: 300,
+    backgroundColor: '#202325',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    alignItems: 'center',
+  },
+
+  buttonClose: {
+    backgroundColor: Colors.red.error,
+  },
+  modalTextStyle: {
+    color: '#979c9e',
+    fontFamily: 'Inter-Regular',
+    fontSize: 17,
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    fontFamily: 'Inter-Bold',
+    fontSize: 24,
+    textAlign: 'center',
+    color: 'white',
   },
 });
 export default HomeCheckedIn;
