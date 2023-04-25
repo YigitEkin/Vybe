0921f7d0954d0789d1db9a9ede5a9e0ae4d28c34
YigitEkin <yigit.ekin@ug.bilkent.edu.tr>
2023-04-25 19:14:08 +0300

    login & logout store binding

--- frontend/stores/LoginStore.ts
+++ frontend/stores/LoginStore.ts
@@ -1,11 +1,15 @@
 import create from "zustand";
 
 export type LoginStore = {
+  isLogin: boolean;
+  setIsLogin: (isLogin: boolean) => void;
   phoneNumber: string | null;
   setPhoneNumber: (phoneNumber: string | null) => void;
 };
 
-export const useLoginStore = create((set) => ({
+export const useLoginStore = create<LoginStore>((set) => ({
   phoneNumber: null,
   setPhoneNumber: (phoneNumber: string | null) => set({ phoneNumber }),
+  isLogin: false,
+  setIsLogin: (isLogin: boolean) => set({ isLogin }),
 }));
