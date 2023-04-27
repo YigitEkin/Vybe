747ade2f4af31d99ad77222ef19e2a2bc0db63a8
Can Önal <can.onal@ug.bilkent.edu.tr>
2023-04-27 18:10:19 +0300

    stripe dismissed

--- frontend/screens/homePages/settings/PaymentByCard.tsx
+++ frontend/screens/homePages/settings/PaymentByCard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StripeProvider, usePaymentSheet } from '@stripe/stripe-react-native';
+
 import {
   View,
   Text,
@@ -13,32 +13,9 @@ import {
 import StyledButton from '../../../components/HomePage/StyledButton';
 
 const PaymentByCard = () => {
-  const { initPaymentSheet, presentPaymentSheet, loading } = usePaymentSheet();
-  const [ready, setReady] = useState(false);
-
-  useEffect(() => {
-    initialisePaymentSheet();
-  }, []);
-
-  const initialisePaymentSheet = async () => {
-    const;
-  };
-
-  async function buy() {
-    const { error } = await presentPaymentSheet();
-
-    if (error) {
-      Alert.alert(`Error code: ${error.code}`, error.message);
-    } else {
-      Alert.alert('Success', 'The payment was confirmed successfully');
-      setReady(false);
-    }
-  }
   return (
     <View style={{ flex: 1 }}>
-      <StripeProvider publishableKey='pk_test_51N19G4Fn58HWM8DI9CdqhEL84LSRM5xjkKdKdxgxmrQLDFpLBGAw113LmN6tzbv2RdZimMJnb4ZcrWblQt9MvKdb00hto3rLCu'>
-        <StyledButton buttonText={'buy'}>Buy</StyledButton>
-      </StripeProvider>
+      <StyledButton buttonText={'buy'}>Buy</StyledButton>
     </View>
   );
 };
