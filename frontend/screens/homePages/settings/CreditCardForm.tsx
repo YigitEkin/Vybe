7801a0e585d9c0c7d66322b95e25d33de8ac9fdd
Can Önal <can.onal@ug.bilkent.edu.tr>
2023-04-27 19:40:13 +0300

    card form component added

--- frontend/screens/homePages/settings/CreditCardForm.tsx
+++ frontend/screens/homePages/settings/CreditCardForm.tsx
@@ -1,11 +1,24 @@
-//import {
-//  CreditCardInput,
-//  LiteCreditCardInput,
-//} from 'react-native-credit-card-input-plus';
+import {
+  CreditCardInput,
+  LiteCreditCardInput,
+} from 'react-native-credit-card-input-plus';
 import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
+import * as Font from 'expo-font';
 
 const CreditCardForm = () => {
-  return <View style={{ flex: 1 }}>{/*<CreditCardInput onCha />*/}</View>;
+  const [fontsLoaded] = Font.useFonts({
+    'Inter-Regular': require('../../../assets/fonts/Inter/static/Inter-Regular.ttf'),
+  });
+  return (
+    <View style={{ flex: 1 }}>
+      <CreditCardInput
+        requiesName={true}
+        inputStyle={{ color: 'white' }}
+        labelStyle={{ color: 'white' }}
+        cardFontStyle='Inter'
+      />
+    </View>
+  );
 };
 export default CreditCardForm;
